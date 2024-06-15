5ecd5ea083b8fe35a23fc34d95e3dea06c49b753
Savit-Raj <btech10303.22@bitmesra.ac.in>
2024-06-15 20:58:09 +0530

    Updated

--- Attendance.py
+++ Attendance.py
@@ -43,6 +43,7 @@ print('Encoding Complete')
 cap = cv2.VideoCapture(0)
 while True:
     success, img = cap.read()
+    img = cv2.flip(img, 1)
     imgS = cv2.resize(img,(0,0),None,0.25,0.25)
     imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
 
@@ -64,6 +65,5 @@ while True:
             cv2.rectangle(img, (x1,y1),(x2,y2),(0,255,0))
             cv2.putText(img,name,(x1+6,y2-6), cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
             markAttendance(name)
-
     cv2.imshow('Webcam',img)
     cv2.waitKey(1)
