7f7f7f97296ad74bd7ecc00ab4f4e0dc1cbf9245
Savit-Raj <btech10303.22@bitmesra.ac.in>
2024-06-18 20:42:22 +0530

    Unknown Faces Update

--- Attendance.py
+++ Attendance.py
@@ -56,14 +56,19 @@ while True:
         # print(faceDis)
         matchIndex = np.argmin(faceDis)
 
-        if matches[matchIndex]:
+        if matches[matchIndex] and faceDis[matchIndex] < 0.3:
             name = classNames[matchIndex].upper()
-            # print(name)
-            y1,x2,y2,x1 = faceLoc
-            y1, x2, y2, x1 = y1*4, x2*4, y2*4, x1*4
-            cv2.rectangle(img, (x1,y1),(x2,y2),(0,255,0),2)
-            cv2.rectangle(img, (x1,y1),(x2,y2),(0,255,0))
-            cv2.putText(img,name,(x1+6,y2-6), cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
+        else:
+            name = 'UNKNOWN'
+
+        y1, x2, y2, x1 = faceLoc
+        y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+        cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0))
+        cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+
+        if name != 'UNKNOWN':
             markAttendance(name)
+
     cv2.imshow('Webcam',img)
     cv2.waitKey(1)
